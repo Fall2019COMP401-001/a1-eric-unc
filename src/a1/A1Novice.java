df936b907473a3aae2a8e4ccd954f6d8ea905d82
u1643364 <u1643364@u1643364-pc.wireless-1x.unc.edu>
2019-08-25 21:10:32 -0400

    cross fingers

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -1,14 +1,39 @@
 package a1;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class A1Novice {
 
-	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
+	@SuppressWarnings("resource")
+	public static void main(String[] args){
+		var scan = new Scanner(System.in);
 
-		// Your code follows here.
+		var numCustomers = scan.nextInt();
 		
+		HashMap<String, Double> customers = new HashMap<>();
+		
+		for(int i = 0; i <= numCustomers - 1; i++){
+			var firstName = scan.next();
+			var lastName = scan.next();
+			
+			var name = firstName.charAt(0) + ". " + lastName;
+			
+			customers.put(name, 0.0);
+			
+			var numItems = scan.nextInt();
+			
+			for(int j = 0; j <= numItems - 1; j++){
+				var numOfItem = scan.nextInt();
+				scan.next(); //The name of the item isn't important
+				var price = scan.nextDouble();
+				
+				customers.put(name, customers.get(name) + (numOfItem * price));
+			}
+		}
+		
+		customers.forEach((name, cost) -> {
+			System.out.println(name + ": " + cost);
+		});
 	}
 }
